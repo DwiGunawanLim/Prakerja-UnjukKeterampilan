761c86bc23fd890f683ebcd2d70e71ba2fbc1d1a
Dwi Gunawan <dwigunawanlim@gmail.com>
2023-11-05 14:52:50 +0700

    simple routing

---
+++ index.js
@@ -0,0 +1,32 @@
+import * as CustomerService from './services/customer.js';
+import * as ProductService from './services/product.js';
+import * as OrderService from './services/order.js';
+import express from 'express';
+
+const host = "localhost";
+const port = 8080;
+const app = express();
+
+app.use(express.json()); //ngehandle js nya
+
+// Handle Routing Customer
+app.get("/customers/profile", CustomerService.validateToken, CustomerService.getCustomerProfile);
+app.post("/customers", CustomerService.createCustomer);
+app.put("/customers/profile/edit", CustomerService.validateToken, CustomerService.updateCustomerProfile);
+app.put("/customers/profile/edit/password", CustomerService.validateToken, CustomerService.updateCustomerPass);
+app.post("/login", CustomerService.login);
+
+// Handle Routing Product
+app.post("/products", CustomerService.validateToken, ProductService.createProduct);
+app.get("/products/detail/:nama_produk", CustomerService.validateToken, ProductService.getProductDetail);
+app.put("/products/detail/edit", CustomerService.validateToken, ProductService.updateProductDetail);
+app.delete("/products/:id", CustomerService.validateToken, ProductService.deleteProduct);
+app.get("/products", CustomerService.validateToken, ProductService.getProduct);
+app.get("/products/search/:categories", CustomerService.validateToken, ProductService.getProductbyCategories);
+
+// Handle Order
+app.post("/orders", CustomerService.validateToken, OrderService.createOrder);
+
+app.listen(port, host, () => {
+    console.log(`server berjalan di http://${host}:${port}`);
+})
